51ebf66c78436b011f236ddfa813eebfb7b74f21
shhrrtnvr <shhrrtnvr@gmail.com>
2020-07-15 21:01:41 +0600

    Week 2 class 2

---
+++ src/Coordinate.java
@@ -0,0 +1,14 @@
+class Point {
+    private int x, y;
+    public Point(int a, int b)
+    {
+       x = a;
+       y = b;
+    }
+}
+public class Coordinate{
+    public static void main(String[] args)
+    {
+        Point p = new Point(1,1);
+    }
+}
