00c5e05865070d04b9117f1dd6dccb6dcb60d406
shhrrtnvr <shhrrtnvr@gmail.com>
2020-07-20 14:33:35 +0600

    integer parsed

---
+++ src/string_parse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class string_parse {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String s1 = new String();
+        s1 = sc.nextLine();
+
+        String a = new String();
+        String b = new String();
+        a = s1.substring(0, 1);
+        b = s1.substring(2);
+        System.out.println(Integer.parseInt(a) + Integer.parseInt(b));
+
+
+    }
+}
