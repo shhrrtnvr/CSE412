93335821fb0bd2fa3bd9b751d1f831a081e0f759
shhrrtnvr <shhrrtnvr@gmail.com>
2020-07-27 13:56:48 +0600

    parent and child methods made non static

--- src/child.java
+++ src/child.java
@@ -1,14 +1,15 @@
 public class child extends parent {
-    public static int sum(int a)
+    @Override
+    public int sum(int a)
     {
         return a*a;
     }
     public static void main(String[] args)
     {
-
-        System.out.println(sum(2)); //overriding
-        System.out.println(sum(1, 2)); // overloading of sum
-        System.out.println(sum(1,2,3)); //overloading of sum
+        child y = new child();
+        System.out.println(y.sum(2)); //overriding
+        System.out.println(y.sum(1, 2)); // overloading of sum
+        System.out.println(y.sum(1,2,3)); //overloading of sum
 
     }
 }
--- src/parent.java
+++ src/parent.java
@@ -1,20 +1,21 @@
 public class parent {
     private int data;
-    public static int sum(int a)
+    public int sum(int a)
     {
         return a;
     }
-    public static int sum(int a, int b)
+    public int sum(int a, int b)
     {
         return a+b;
     }
-    public static int sum(int a, int b, int c)
+    public int sum(int a, int b, int c)
     {
         return a+b+c;
     }
     public static void main(String[] args)
     {
-        System.out.println(sum(1,2));
-        System.out.println(sum(1,2,3));
+        parent x = new parent();
+        System.out.println(x.sum(1,2));
+        System.out.println(x.sum(1,2,3));
     }
 }
