2185b5e0915e94bbeb2d9c944de96f7b890c6a76
shhrrtnvr <shhrrtnvr@gmail.com>
2020-07-27 14:22:31 +0600

    X and Y class modified with constructor

--- src/Y.java
+++ src/Y.java
@@ -1,19 +1,24 @@
 public class Y extends X {
     private int  w, z;
-    public void set(int x, int y, int w, int z)
+    public Y(int x, int y)
     {
         super.set(x, y);
+    }
+    @Override
+    public void set(int w, int z)
+    {
         this.w = w;
         this.z = z;
     }
+    @Override
     public int get()
     {
         return super.get()*(z*z - w*w);
     }
     public static void main(String[] args)
     {
-        Y y = new Y();
-        y.set(2,1, 3, 4);
+        Y y = new Y(1,2);
+        y.set(3, 4);
         System.out.println(y.get());
     }
 }
