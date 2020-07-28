cf935eeafae9bef88c63276d5a8b38d83a1f1271
shhrrtnvr <shhrrtnvr@gmail.com>
2020-07-28 10:34:48 +0600

    permutation added in combination

--- src/combination.java
+++ src/combination.java
@@ -5,9 +5,15 @@ public class combination {
         if(r == 0) return 1;
         return comb(n-1, r) + comb(n-1, r-1);
     }
+    private int factorial(int n)
+    {
+        if (n == 0) return 1;
+        return n * factorial(n-1);
+    }
+
     public static void main(String[] args)
     {
         combination c = new combination();
-        System.out.println(c.comb(15,13));
+        System.out.println(c.comb(4,2) * c.factorial(2));
     }
 }
