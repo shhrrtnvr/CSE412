44c45e497d15f9b2656d5b433d7c2c3c303d6bbf
shhrrtnvr <shhrrtnvr@gmail.com>
2020-08-04 10:40:21 +0600

    fibonacci_recursive added

--- src/fibonacci_recursive.java
+++ src/fibonacci_recursive.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 public class fibonacci_recursive {
     public int fib(int n)
     {
-        if (n==0) return 0;
-        if(n==1) return 1;
+        if (n==0 || n==1) return 1;
         return fib(n-1) + fib(n-2);
     }
     public static void main(String[] args)
