cdc6372a6d885317e6696042c7bbcacc7f6353b5
shhrrtnvr <shhrrtnvr@gmail.com>
2020-09-01 11:09:04 +0600

    io clean coded

--- src/FileInputSum.java
+++ src/FileInputSum.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 public class FileInputSum {
     public static void main(String[] args)
     {
+        FileInputStream f1, f2;
+        PrintWriter pw;
         try {
-            FileInputStream f1 = new FileInputStream("files/a");
-            FileInputStream f2 = new FileInputStream("files/b");
+            f1 = new FileInputStream("files/a");
+            f2 = new FileInputStream("files/b");
 
             Scanner sc1 = new Scanner(f1);
             Scanner sc2 = new Scanner(f2);
 
-            PrintWriter pw = new PrintWriter(new File("files/o"));
+            pw = new PrintWriter(new File("files/o"));
             pw.write(Integer.toString(sc1.nextInt() + sc2.nextInt()));
             pw.flush();
-            pw.close();;
+            pw.close();
         } catch (FileNotFoundException e) {
             System.out.println("Error while reading file");
         }
